9c62b25fe17543dd9c566d4275396ff39a1fa159
sebasmzg <sebasmz15@gmail.com>
2024-08-12 08:20:03 -0500

    first page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+export default function Contact() {
+  return (
+    <main className="">
+      <h1>Home Page</h1>
+      <p>Welcome to the Home page.</p>
+      <Link href="/">Go to About Page</Link>
+    </main>
+  );
+}
